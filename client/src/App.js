75ac39efcc498489034c7235c7f504267f308a93
andytheelf <ballew22@gmail.com>
2020-08-27 15:29:12 -0500

    test

---
+++ client/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Nav';
+import Request from './pages/Request';
+import About from './pages/About';
+// import Contact from './components/Contact';
+import Donations from './pages/Donations';
+import Footer from './components/Footer';
+// import Header from './components/Header';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import SingleRequest from './pages/SingleRequest';
+import NoMatch from './pages/NoMatch';
+
+
+
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={About} />
+              <Route exact path="/about" component={About} />
+              <Route exact path="/donations" component={Donations} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/request" component={Request} />
+              <Route exact path="/request/:id" component={SingleRequest} />
+              <Route exact path="/profile/:username?" component={Profile} />
+
+              <Route component={NoMatch} />
+            </Switch>
+          </>
+          <Footer />
+        </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import Auth from '../../utils/auth';
+
+function Navigation() {
+
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+      };
+
+    return (
+        <Navbar className="mr-auto">
+            <Navbar.Brand href="/">
+                <img 
+                    src="https://img.icons8.com/cotton/64/000000/volunteering.png" 
+                    alt='help-exchange'
+                    className="justify-space-between"
+                /> Help Exchange
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="">
+                <Nav.Link className='nav-link' href="/about">
+                    <img 
+                        src="https://img.icons8.com/ios-filled/50/000000/info.png"
+                        alt='about'
+                        width='50'
+                        height='50'
+                        />
+                        About Us
+                </Nav.Link>
+                {/* <Nav.Link className='nav-link' href="/Volunteer">
+                    <img src="https://img.icons8.com/cotton/64/000000/volunteering.png"
+                    alt='volunteer'
+                    width='50'
+                    height='50'/>
+                        Volunteer
+                </Nav.Link> */}
+                <Nav.Link className='nav-link' href="/request">
+                    <img src="https://img.icons8.com/ios-filled/50/000000/connectivity-and-help.png"
+                    alt='request-help'
+                    width='50'
+                    height='50'
+                    />
+                        Request Help 
+                </Nav.Link>
+                <Nav.Link className='nav-link' href="/donations">
+                    <img src="https://img.icons8.com/ios-filled/50/000000/charity.png"
+                    alt='donations'
+                    width='50'
+                    height='50'
+                    />
+                        Donations
+                </Nav.Link>
+                <NavDropdown title="Account" id="login" className="align-self-center">
+                        {Auth.loggedIn() ? (
+                                <>
+                                <NavDropdown.Item href="/profile">Me</NavDropdown.Item>
+                                <NavDropdown.Item href="/" onClick={logout}>
+                                    Logout
+                                </NavDropdown.Item>
+                                </>
+                            ) : (
+                                <>
+                                <NavDropdown.Item href="/login">
+                                        {/* <img src="https://img.icons8.com/ios-filled/50/000000/login-rounded-right.png"
+                                        alt='login'
+                                        width='50'
+                                        height='50'/> */}
+                                            Login
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="/signup">
+                                        <img src="https://img.icons8.com/ios-filled/50/000000/login-rounded-right.png"
+                                        alt='login'
+                                        width='50'
+                                        height='50'
+                                        />
+                                            Sign Up
+                                </NavDropdown.Item>
+                                </>
+                            )}
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+
+
+export default Navigation;
